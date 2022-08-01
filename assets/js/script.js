af5c65732e4994fbd7ba85f21cd2d68c09babf9a
David Barraza <dbcomps@mac.com>
2022-08-01 18:49:18 -0500

    criteria comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,11 @@ console.log("The button was clicked");
 
 return "password";
 };
+
+// presented user with prompts for password criteria
+// 1. prompt for the length of the password. between 8 and 128 characters
+// 2. prompt for including types of characters
+	// a. make an array of types of characters?
+			// lowercase, uppercase, numeric, and/or special characters
+// 3. validate input and at least one character type should be chosen
+// 4. return password
